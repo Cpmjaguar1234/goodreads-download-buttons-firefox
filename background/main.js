c570c21c6d6a4570134d456124987b24b53940ff
Cpmjaguar1234 <93066433+Cpmjaguar1234@users.noreply.github.com>
2024-12-26 22:23:34 -0500

    Change manifest from v3 to v2 - Replace chrome.* APIs with browser.* APIs - Remove scripting permission and programmatic content script injection - Update content script matches to support both http and https - Change action to browser_action

--- background/main.js
+++ background/main.js
@@ -1,10 +1,5 @@
-chrome.webNavigation.onCompleted.addListener((details) => {
+browser.webNavigation.onCompleted.addListener((details) => {
     if (details.frameId !== 0) {
         return;
     }
-
-    chrome.scripting.executeScript({
-        target: { tabId: details.tabId },
-        files: ["content/main.js"]
-    });
 }, { url: [{ schemes: ['http', 'https'] }] });
